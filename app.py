5f1821725dfb73d1c40c8debb9bc9109557f75e7
Rishav-hub <9930046@gmail.com>
2022-04-13 20:35:55 +0530

    updated port

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 import os
 if __name__ == '__main__':
-    os.system('uvicorn project_store_presentation_layer.endpoints:app --reload')
+    # os.system('uvicorn project_store_presentation_layer.endpoints:app --reload --host=0.0.0.0 --port=8080')
+    os.system('uvicorn project_store_presentation_layer.endpoints:app --reload --host=127.0.0.1 --port=8080')
